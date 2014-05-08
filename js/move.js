109678e474ad7c6074a25366260dfd3ff5be5ddf
xushuai8967 <shuaixu8967@163.com>
2014-05-09 00:02:28 +0800

    修改move.js为通用运动，支持属性传入

--- js/move.js
+++ js/move.js
@@ -6,18 +6,22 @@ window.onload = function() {
 	for (var i = 0; i < divs.length; i++) {
 		//初始化所有div
 		divs[i].timer = null;
-	    new MoveDiv(divs[i]);
+	    new MoveDiv(divs[i],"width");
 	}
 };
 
-function MoveDiv(div,target) {
+function MoveDiv(div, attr, target) {
 	//构造函数
 	this.div = div;
+	this.attr = attr;
 	this.moveTo = target || 350;
-	this.initOffsetWidth = this.div.offsetWidth;
+	//this.initOffsetWidth = this.div.offsetWidth;
+	this.initStyle = parseInt(this.getStyle());
 	//为该对象增加运动事件
 	this.startMove();
 }
+
+
 
 MoveDiv.prototype.startMove = function() {
 	var _this = this;
@@ -26,7 +30,7 @@ MoveDiv.prototype.startMove = function() {
 	};
 
 	_this.div.onmouseout = function() {
-		_this.move(_this.initOffsetWidth);
+		_this.move(_this.initStyle);
 	};
 };
 
@@ -35,12 +39,21 @@ MoveDiv.prototype.move = function(target) {
 	clearInterval(_this.div.timer);
 
 	_this.div.timer = setInterval(function() {
-		var speed = (target - _this.div.offsetWidth) / 8;
+		var iCur = parseInt(_this.getStyle());
+		var speed = (target - iCur) / 8;
 		speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
-		if (_this.div.offsetWidth == target) {
+		if (iCur == target) {
 			clearInterval(_this.div.timer);
 		} else {
-			_this.div.style.width = _this.div.offsetWidth + speed + "px";
+			_this.div.style[_this.attr] = iCur + speed + "px";
 		}
 	}, 30);
+};
+
+MoveDiv.prototype.getStyle = function () {
+		if(this.div.currentStyle){
+			return this.div.currentStyle[this.attr];
+		} else {
+			return getComputedStyle(this.div, false)[this.attr];
+		}
 };
